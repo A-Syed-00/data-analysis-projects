3fe5adcc6aeddae4c1cc5d7f90ac2d4e8a5b9697
A-Syed-00 <ayb.syed@outlook.com>
2026-02-02 13:04:59 -0500

    Refactor comments and output description in SQL script
    Updated comments for clarity and corrected the output description.

--- Ecommerce-Funnel-Analysis/sql/05_creating_binary_flags.sql
+++ Ecommerce-Funnel-Analysis/sql/05_creating_binary_flags.sql
@@ -1,14 +1,20 @@
 -- ===========================================================================================
--- 01_creating_binary_flags.sql 
--- Purpose: Transforming numeric counts into yes/no flags
---          per user for each event type 
+-- 05_creating_binary_flags.sql 
+-- Purpose: Transform numeric counts into yes/no flags per user for each event type 
 -- Input: events_cleaned table 
--- Output: A table (events_flags) for funnel progression and visualization
+-- Output: events_flags table
+-- Notes: This table will be used for funnel progression and visualization
 -- ===========================================================================================
+DROP TABLE IF EXISTS events_flags; 
+
 CREATE TABLE events_flags AS
-SELECT user_id,
-MAX(CASE WHEN event_type LIKE "%purchase%" THEN 1 ELSE 0 END) AS  has_purchase,
-MAX(CASE WHEN event_type LIKE "%cart%" THEN 1 ELSE 0 END) AS has_carted,
-MAX(CASE WHEN event_type LIKE "%view%" THEN 1 ELSE 0 END) AS has_view
+SELECT 
+  user_id,
+  MAX(CASE WHEN event_type LIKE "%purchase%" THEN 1 ELSE 0 END) AS  has_purchase,
+  MAX(CASE WHEN event_type LIKE "%cart%" THEN 1 ELSE 0 END) AS has_carted,
+  MAX(CASE WHEN event_type LIKE "%view%" THEN 1 ELSE 0 END) AS has_view
 FROM events_cleaned
-GROUP BY user_id
+GROUP BY user_id; 
+
+--Optional sanity check: 
+-- SELECT * FROM events_flags WHERE user_id = 1515915625519356010;
