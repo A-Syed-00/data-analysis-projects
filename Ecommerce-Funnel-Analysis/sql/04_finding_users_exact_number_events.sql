8a0da2d6510f19bb8f7c8b2f883745791bbc525c
A-Syed-00 <ayb.syed@outlook.com>
2026-02-02 12:54:18 -0500

    Create user_event_counts table for user activity analysis
    Create user_event_counts table with event type counts per user.

--- Ecommerce-Funnel-Analysis/sql/04_finding_users_exact_number_events.sql
+++ Ecommerce-Funnel-Analysis/sql/04_finding_users_exact_number_events.sql
@@ -1,15 +1,22 @@
 -- ===========================================================================================
 -- 04_finding_users_exact_number_events.sql 
--- Purpose: Breaking down user activity by event_type: view, cart, purchase
--- Output: Counts for each event type per user
+-- Purpose: Create a user-level table with total counts per event type (view, cart, purchase) 
+-- Input events_cleaned table 
+-- Output: user_event_counts table 
+-- Notes: This table is used for funnel flags and further analysis
 -- ===========================================================================================
 
-SELECT user_id, COUNT(event_type) AS total_number_events,
-SUM(CASE WHEN event_type LIKE "%purchase%" THEN 1 ELSE 0 END) AS number_of_purchases,
-SUM(CASE WHEN event_type LIKE "%cart%" THEN 1 ELSE 0 END) AS number_of_cart,
-SUM(CASE WHEN event_type LIKE "%view%" THEN 1 ELSE 0 END) AS number_of_views
-FROM events_cleaned
-GROUP BY user_id
+DROP TABLE IF EXISTS user_event_counts; 
+
+CREATE TABLE user_event_counts AS 
+SELECT 
+  user_id, 
+  COUNT(event_type) AS total_number_events,
+  SUM(CASE WHEN event_type LIKE "%purchase%" THEN 1 ELSE 0 END) AS number_of_purchases,
+  SUM(CASE WHEN event_type LIKE "%cart%" THEN 1 ELSE 0 END) AS number_of_cart,
+  SUM(CASE WHEN event_type LIKE "%view%" THEN 1 ELSE 0 END) AS number_of_views
+  FROM events_cleaned
+  GROUP BY user_id; 
 
 --Optional: Sanity check 
 -- Add WHERE statement along with a particular user_id to check if counts are accurate: 
