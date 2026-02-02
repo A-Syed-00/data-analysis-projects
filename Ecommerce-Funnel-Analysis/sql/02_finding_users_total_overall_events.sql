f9327cd18c84876314ce6f72352ef113a1b09e07
A-Syed-00 <ayb.syed@outlook.com>
2026-02-02 12:48:16 -0500

    Update comments for clarity in SQL script

--- Ecommerce-Funnel-Analysis/sql/02_finding_users_total_overall_events.sql
+++ Ecommerce-Funnel-Analysis/sql/02_finding_users_total_overall_events.sql
@@ -1,13 +1,15 @@
 -- ===========================================================================================
 -- 02_finding_users_total_overall_events.sql 
--- Purpose: Getting baseline numbers for user engagement        
+-- Purpose: Get baseline numbers for user engagement        
 -- Input: events_cleaned table 
--- Output: Total number of actions (view, cart, purchase) per user  
+-- Output: Total number of actions (view, cart, purchase) per user
+-- Notes: This table is for exploration
 -- ===========================================================================================
 DROP TABLE IF EXISTS user_total_events; 
 
 CREATE TABLE user_total_events AS 
-SELECT user_id,  
+SELECT 
+  user_id,  
   COUNT(event_type) AS total_number_of_events
 FROM events_cleaned
-GROUP BY user_id
+GROUP BY user_id; 
